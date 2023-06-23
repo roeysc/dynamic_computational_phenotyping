c275e0062a386e64aa1bcc76ec0791e3c68da625
reznikda <reznikda@gmail.com>
2023-06-23 16:01:59 +0200

    Add files via upload

---
+++ tasks/PHP/database_config_task.php
@@ -0,0 +1,8 @@
+<?php
+  $servername = "localhost"; // "localhost";
+  $port = 3306;
+  $username = "ussername"; // "username";
+  $password = "password"; // "password";
+  $dbname = "database_name";  // "database";
+  $table = "table_name"; // "tablename";
+?>
