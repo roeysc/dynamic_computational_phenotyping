c8881a590ca0187e8913864f52559173f7071bd3
roeysc <roeysc@gmail.com>
2023-06-26 17:09:06 -0400

    Delete matlab directory

--- matlab/comppheno_set_dirs.m
+++
@@ -1,6 +0,0 @@
-% comppheno_dir = 'SET/PATH/TO/THE/MAIN/MATLAB/DIRECTORYOF/THE/PROJECT'
-comppheno_dir = '/net/rcstorenfs02/ifs/rc_labs/gershman_lab/users/Stan/1_code/publish_code/code/matlab';
-addpath(genpath(comppheno_dir));
-
-comppheno_data_dir = fullfile(fileparts(fileparts(comppheno_dir)),'data');
-comppheno_analysis_dir = fullfile(fileparts(fileparts(comppheno_dir)),'analysis');
